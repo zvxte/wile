83c8c5502d68dbddaa01ec9e3f2e599444ceba7d
zvxte <zvxte.codes@gmail.com>
2024-07-17 14:21:20 +0200

    create parser interface, create chess.com parser implementation

---
+++ src/parser/parser.py
@@ -0,0 +1,56 @@
+from typing import Protocol, Any
+
+from .error import ParserError
+from .utils import parse_pgn_moves
+from ..models import Game, Player
+
+
+class Parser(Protocol):
+    """Parser Interface"""
+
+    def parse(self, game: Any) -> Game:
+        """
+        Parses chess game
+
+        Raises:
+            ParserError
+        """
+        raise NotImplementedError
+
+
+class ChessComParser:
+    def parse(self, game: dict[str, Any]) -> Game:
+        try:
+            return Game(
+                game_id=game["uuid"],
+                platform="Chess.com",
+                url=game["url"],
+                white=Player(
+                    username=game["white"]["username"], rating=game["white"]["rating"]
+                ),
+                black=Player(
+                    username=game["black"]["username"], rating=game["black"]["rating"]
+                ),
+                initial_fen=game["initial_setup"],
+                moves=parse_pgn_moves(game["pgn"]),
+            )
+        except KeyError or ValueError as e:
+            raise ParserError(e)
+
+
+async def main():
+    # testing
+
+    fetcher = ChessComFetcher()
+    games = await fetcher.fetch("hikaru", 1721088000)  # since July 16, 2024
+
+    parser = ChessComParser()
+    for game in games[:5]:
+        print(parser.parse(game), end="\n\n")
+
+
+if __name__ == "__main__":
+    from asyncio import run
+    from ..fetcher import ChessComFetcher
+
+    run(main())
