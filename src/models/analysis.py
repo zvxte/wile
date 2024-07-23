bd34ffbd937e680cb56f232b899a7e786265eeac
zvxte <zvxte.codes@gmail.com>
2024-07-23 23:10:48 +0200

    rename uci_name to uci_move

--- src/models/analysis.py
+++ src/models/analysis.py
@@ -4,15 +4,22 @@ class Analysis:
 
     Raises:
         AssertionError: If arguments with invalid types are provided.
+        ValueError: If argument values are falsy.
     """
 
-    def __init__(self, uci_name: str, multipv: int, score_name: str, score_value: int):
-        assert isinstance(uci_name, str), ["Invalid uci_name type", uci_name]
+    def __init__(self, uci_move: str, multipv: int, score_name: str, score_value: int):
+        assert isinstance(uci_move, str), ["Invalid uci_move type", uci_move]
         assert isinstance(multipv, int), ["Invalid multipv type", multipv]
         assert isinstance(score_name, str), ["Invalid score_name type", score_name]
         assert isinstance(score_value, int), ["Invalid score_value type", score_value]
 
-        self.uci_name = uci_name
+        if not uci_move or not score_name:
+            raise ValueError("Invalid arguments")
+
+        self.uci_move = uci_move
         self.multipv = multipv
         self.score_name = score_name
         self.score_value = score_value
+
+    def __repr__(self) -> str:
+        return f"Analysis({self.uci_move}, {self.multipv}, {self.score_name}, {self.score_value})"
