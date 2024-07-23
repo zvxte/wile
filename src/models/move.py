b231e7ceae49c43224c2814b993dd9502acb3dd6
zvxte <zvxte.codes@gmail.com>
2024-07-23 23:12:53 +0200

    add type checks, argument value checks, __repr__ method

--- src/models/move.py
+++ src/models/move.py
@@ -9,12 +9,41 @@ class Move:
 
     Raises:
         AssertionError: If arguments with invalid types are provided.
+        ValueError: If argument values are falsy.
     """
-    def __init__(self, san_move: str, uci_move: Optional[str] = None, analyses: Optional[list[Analysis]] = None):
+
+    def __init__(
+        self,
+        san_move: str,
+        uci_move: Optional[str] = None,
+        score_name: Optional[str] = None,
+        score_value: Optional[int] = None,
+        analyses: Optional[list[Analysis]] = None,
+    ):
         assert isinstance(san_move, str), ["Invalid san_move type", san_move]
-        assert isinstance(uci_move, Union[str, None]), ["Invalid uci_move type", uci_move]
-        assert isinstance(analyses, Union[list, None]), ["Invalid analyses type", analyses]
+        assert isinstance(uci_move, Union[str, None]), [
+            "Invalid uci_move type",
+            uci_move,
+        ]
+        assert isinstance(score_name, Union[str, None]), [
+            "Invalid score_name type",
+            score_name,
+        ]
+        assert isinstance(score_value, Union[int, None]), [
+            "Invalid score_value type",
+            score_value,
+        ]
+        assert isinstance(analyses, Union[list, None]), [
+            "Invalid analyses type",
+            analyses,
+        ]
+
+        if not san_move:
+            raise ValueError("Invalid arguments")
 
         self.san_move = san_move
         self.uci_move = uci_move
         self.analyses = analyses
+
+    def __repr__(self) -> str:
+        return f"Move({self.san_move}, {self.uci_move}, {self.analyses})"
