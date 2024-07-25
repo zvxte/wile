4a3031d20b7f1740aabbcbe97f78240f824eeefc
zvxte <zvxte.codes@gmail.com>
2024-07-25 23:53:25 +0200

    add initial_fen argument to analyze() method

--- src/engine/engine.py
+++ src/engine/engine.py
@@ -4,11 +4,12 @@ from typing import Protocol, Iterable, Any
 class Engine(Protocol):
     """Engine Interface"""
 
-    async def analyze(self, uci_moves: Iterable[Any]) -> Iterable[Any]:
+    async def analyze(self, initial_fen: str, uci_moves: Iterable[Any]) -> Iterable[Any]:
         """
         Analyzes chess game
 
         Args:
+            initial_fen (str): Initial position in FEN.
             uci_moves (Iterable[Any]): Moves to analyze in UCI notation.
 
         Returns:
