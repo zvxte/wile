ef2cc838b8737a9ec85ab7c52f1153f730fd741c
zvxte <zvxte.codes@gmail.com>
2024-07-30 23:42:56 +0200

    create move related structures and enumerations, declare functions

--- src/chess/chess_c/types.h
+++ src/chess/chess_c/types.h
@@ -4,7 +4,7 @@
 
 #include <stdint.h>
 
-typedef uint64_t Bitboard;
+typedef uint64_t Bitboard_t;
 
 typedef enum Square {
   SQUARE_A1, SQUARE_B1, SQUARE_C1, SQUARE_D1, SQUARE_E1, SQUARE_F1, SQUARE_G1, SQUARE_H1,
@@ -16,6 +16,7 @@ typedef enum Square {
   SQUARE_A7, SQUARE_B7, SQUARE_C7, SQUARE_D7, SQUARE_E7, SQUARE_F7, SQUARE_G7, SQUARE_H7,
   SQUARE_A8, SQUARE_B8, SQUARE_C8, SQUARE_D8, SQUARE_E8, SQUARE_F8, SQUARE_G8, SQUARE_H8,
   SQUARE_NONE,
+  SQUARE_MAX = 64,
 } Square_t;
 
 typedef enum Color {
@@ -76,12 +77,60 @@ typedef enum MoveType {
   MOVE_TYPE_CASTLING,
 } MoveType_t;
 
+typedef enum MoveSymbol {
+  MOVE_SYMBOL_NONE,
+  MOVE_SYMBOL_CHECK,
+  MOVE_SYMBOL_MATE,
+} MoveSymbol_t;
+
 typedef struct Move {
   Square_t source_square;
   Square_t target_square;
   Piece_t promotion_piece;
   MoveType_t move_type;
+  MoveSymbol_t move_symbol;
 } Move_t;
+
+typedef struct Position {
+  Bitboard_t occupancy;
+
+  Bitboard_t white_pawns;
+  Bitboard_t white_knights;
+  Bitboard_t white_bishops;
+  Bitboard_t white_rooks;
+  Bitboard_t white_queens;
+  Bitboard_t white_king;
+
+  Bitboard_t black_pawns;
+  Bitboard_t black_knights;
+  Bitboard_t black_bishops;
+  Bitboard_t black_rooks;
+  Bitboard_t black_queens;
+  Bitboard_t black_king;
+
+  Color_t active_color;
+  CastlingRights_t castling_rights;
+  Square_t en_passant_square;
+  int halfmove_clock;
+  int fullmove_number;
+} Position_t;
+
+
+
+Bitboard_t bitboard_create_from_square(Square_t square);
+void bitboard_print(Bitboard_t bitboard);
+
+Square_t square_create_from_file_rank(File_t file, Rank_t rank);
+
+File_t file_create_from_char(char character);
+
+Rank_t rank_create_from_char(char character);
+
+Position_t position_create();
+Position_t position_create_empty();
+Position_t position_create_from_fen(const char *fen);
+int position_move(Position_t *position, const Move_t *move);
+char* position_get_fen(const Position_t *position);
 
 #endif
 // ====TYPES_H==== //
