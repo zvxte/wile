2fed7d2dddeaee669a3d5d65f2c667908db4e1e9
zvxte <zvxte.codes@gmail.com>
2024-07-31 06:29:22 +0200

    implement file_create_from_square() and file_to_char() functions

--- src/chess/chess_c/file.c
+++ src/chess/chess_c/file.c
@@ -3,3 +3,9 @@
 File_t file_create_from_char(char character) {
   return (File_t)(character - 'a');
 };
+
+File_t file_create_from_square(Square_t square) {
+  return (File_t)(square & 7);
+};
+
+char file_to_char(File_t file) { return (char)(file + 'a'); };
