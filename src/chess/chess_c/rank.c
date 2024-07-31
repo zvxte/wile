5a03111bf963dba9a7834b8a045706c0e4d16555
zvxte <zvxte.codes@gmail.com>
2024-07-31 06:29:40 +0200

    implement rank_create_from_square() and rank_to_char() functions

--- src/chess/chess_c/rank.c
+++ src/chess/chess_c/rank.c
@@ -3,3 +3,9 @@
 Rank_t rank_create_from_char(char character) {
   return (Rank_t)(character - '1');
 };
+
+Rank_t rank_create_from_square(Square_t square) {
+  return (Rank_t)(square >> 3);
+};
+
+char rank_to_char(Rank_t rank) { return (char)(rank + '1'); };
