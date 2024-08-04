52cc9b07dacba57681d0d7027831ce2ab821c4fc
zvxte <zvxte.codes@gmail.com>
2024-08-04 18:06:55 +0200

    use Platform enum, add side that represents player's side

--- src/models/game.py
+++ src/models/game.py
@@ -1,5 +1,7 @@
 from .player import Player
 from .move import Move
+from .color import Color
+from .platform import Platform
 
 
 class Game:
@@ -14,22 +16,24 @@ class Game:
     def __init__(
         self,
         game_id: str,
-        platform: str,
+        platform: Platform,
         url: str,
         white: Player,
         black: Player,
+        side: Color,
         initial_fen: str,
         moves: list[Move]
     ):
         assert isinstance(game_id, str), ["Invalid game_id type", game_id]
-        assert isinstance(platform, str), ["Invalid platform type", platform]
+        assert isinstance(platform, Platform), ["Invalid platform type", platform]
         assert isinstance(url, str), ["Invalid url type", url]
         assert isinstance(white, Player), ["Invalid white type", white]
         assert isinstance(black, Player), ["Invalid black type", black]
+        assert isinstance(side, Color), ["Invalid side type", side]
         assert isinstance(initial_fen, str), ["Invalid initial_fen type", initial_fen]
         assert isinstance(moves, list), ["Invalid moves type", moves]
 
-        if not game_id or not platform or not url or not initial_fen or not moves:
+        if not game_id or not url or not initial_fen or not moves:
             raise ValueError("Invalid arguments")
 
         self.game_id = game_id
@@ -37,8 +41,9 @@ class Game:
         self.url = url
         self.white = white
         self.black = black
+        self.side = side
         self.initial_fen = initial_fen
         self.moves = moves
 
     def __repr__(self) -> str:
-        return f"Game(\n{self.game_id}\n{self.platform}\n{self.url}\n{self.white}\n{self.black}\n{self.initial_fen}\n{self.moves}\n)"
+        return f"Game(\n{self.game_id}\n{self.platform}\n{self.url}\n{self.white}\n{self.black}\n{self.side}\n{self.initial_fen}\n{self.moves}\n)"
