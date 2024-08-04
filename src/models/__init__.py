e47dbcf3a825856d66da9cbea84c4c9c4ba81d7d
zvxte <zvxte.codes@gmail.com>
2024-08-04 18:08:34 +0200

    add Puzzle, Color, Platform and ScoreName

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,3 +2,7 @@ from .game import Game
 from .player import Player
 from .move import Move
 from .analysis import Analysis
+from .puzzle import Puzzle
+from .color import Color
+from .platform import Platform
+from .score import ScoreName
