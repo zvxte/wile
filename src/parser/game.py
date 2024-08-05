f4b5cfa8da08f5772274d2a6ab50b62cfb5c17da
zvxte <zvxte.codes@gmail.com>
2024-08-05 17:42:07 +0200

    add username argument to determine player's side

--- src/parser/game.py
+++ src/parser/game.py
@@ -2,18 +2,19 @@ from typing import Protocol, Any
 
 from .error import ParserError
 from .utils import parse_pgn_moves
-from ..models import Game, Player, Move
+from ..models import Game, Player, Move, Color, Platform
 
 
 class GameParser(Protocol):
     """Game Parser Interface"""
 
-    def parse(self, game: Any) -> Game:
+    def parse(self, game: Any, username: str) -> Game:
         """
         Parses chess game
 
         Args:
             game (Any): Game to parse.
+            username (str): Username to determine player's side.
 
         Returns:
             Game: Parsed game.
@@ -26,23 +27,38 @@ class GameParser(Protocol):
 
 
 class ChessComGameParser:
-    def parse(self, game: dict[str, Any]) -> Game:
+    def parse(self, game: dict[str, Any], username: str) -> Game:
         assert isinstance(game, dict), ["Invalid game type", game]
+        assert isinstance(username, str), ["Invalid username type", username]
 
         moves = [Move(san_move) for san_move in parse_pgn_moves(game["pgn"])]
+        for (i, move) in enumerate(moves):
+            move.side = Color.WHITE if i % 2 == 0 else Color.BLACK
+        
+        white = Player(
+            username=game["white"]["username"], rating=game["white"]["rating"]
+        )
+        black = Player(
+            username=game["black"]["username"], rating=game["black"]["rating"]
+        )
+    
+        if white.username.lower() == username.lower():
+            side = Color.WHITE
+        elif black.username.lower() == username.lower():
+            side = Color.BLACK
+        else:
+            raise ParserError("Invalid username")
+
         try:
             return Game(
                 game_id=game["uuid"],
-                platform="Chess.com",
+                platform=Platform.CHESSCOM,
                 url=game["url"],
-                white=Player(
-                    username=game["white"]["username"], rating=game["white"]["rating"]
-                ),
-                black=Player(
-                    username=game["black"]["username"], rating=game["black"]["rating"]
-                ),
+                white=white,
+                black=black,
+                side=side,
                 initial_fen=game["initial_setup"],
-                moves=moves
+                moves=moves,
             )
         except (KeyError, ValueError) as e:
             raise ParserError(e)
@@ -55,7 +71,7 @@ async def main():
 
     parser = ChessComGameParser()
     for game in games[:5]:
-        print(parser.parse(game), end="\n\n")
+        print(parser.parse(game, "hikaru"), end="\n\n")
 
 
 if __name__ == "__main__":
