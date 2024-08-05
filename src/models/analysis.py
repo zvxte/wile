aad6d2bf4a6d89aafb85fc7922dcaba3e4c5fb52
zvxte <zvxte.codes@gmail.com>
2024-08-05 17:39:03 +0200

    use Score type

--- src/models/analysis.py
+++ src/models/analysis.py
@@ -1,4 +1,4 @@
-from .score import ScoreName
+from .score import Score
 
 
 class Analysis:
@@ -10,19 +10,17 @@ class Analysis:
         ValueError: If argument values are falsy.
     """
 
-    def __init__(self, uci_move: str, multipv: int, score_name: ScoreName, score_value: int):
+    def __init__(self, uci_move: str, multipv: int, score: Score):
         assert isinstance(uci_move, str), ["Invalid uci_move type", uci_move]
         assert isinstance(multipv, int), ["Invalid multipv type", multipv]
-        assert isinstance(score_name, ScoreName), ["Invalid score_name type", score_name]
-        assert isinstance(score_value, int), ["Invalid score_value type", score_value]
+        assert isinstance(score, Score), ["Invalid score type", score]
 
         if not uci_move:
             raise ValueError("Invalid arguments")
 
         self.uci_move = uci_move
         self.multipv = multipv
-        self.score_name = score_name
-        self.score_value = score_value
+        self.score = score
 
     def __repr__(self) -> str:
-        return f"Analysis({self.uci_move}, {self.multipv}, {self.score_name}, {self.score_value})"
+        return f"Analysis({self.uci_move}, {self.multipv}, {self.score})"
