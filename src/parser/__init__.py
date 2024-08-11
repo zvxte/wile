083c8464b4e5a68de6f827d882257a4ac91599cb
zvxte <zvxte.codes@gmail.com>
2024-08-11 22:17:20 +0200

    create Lichess.org game parser implementation

--- src/parser/__init__.py
+++ src/parser/__init__.py
@@ -1,3 +1,3 @@
-from .game import GameParser, ChessComGameParser 
+from .game import GameParser, ChessComGameParser , LichessGameParser
 from .analysis import AnalysisParser, StockfishAnalysisParser
 from .error import ParserError
--- src/parser/game.py
+++ src/parser/game.py
@@ -32,16 +32,19 @@ class ChessComGameParser:
         assert isinstance(username, str), ["Invalid username type", username]
 
         moves = [Move(san_move) for san_move in parse_pgn_moves(game["pgn"])]
-        for (i, move) in enumerate(moves):
+        for i, move in enumerate(moves):
             move.side = Color.WHITE if i % 2 == 0 else Color.BLACK
-        
-        white = Player(
-            username=game["white"]["username"], rating=game["white"]["rating"]
-        )
-        black = Player(
-            username=game["black"]["username"], rating=game["black"]["rating"]
-        )
-    
+
+        try:
+            white = Player(
+                username=game["white"]["username"], rating=game["white"]["rating"]
+            )
+            black = Player(
+                username=game["black"]["username"], rating=game["black"]["rating"]
+            )
+        except KeyError as e:
+            raise ParserError(e)
+
         if white.username.lower() == username.lower():
             side = Color.WHITE
         elif black.username.lower() == username.lower():
@@ -64,18 +67,68 @@ class ChessComGameParser:
             raise ParserError(e)
 
 
+class LichessGameParser:
+    def parse(self, game: Any, username: str) -> Game:
+        assert isinstance(game, dict), ["Invalid game type", game]
+        assert isinstance(username, str), ["Invalid username type", username]
+
+        try:
+            moves = [Move(san_move) for san_move in game["moves"].split(" ")]
+            white = Player(
+                game["players"]["white"]["user"]["name"],
+                game["players"]["white"]["rating"],
+            )
+            black = Player(
+                game["players"]["black"]["user"]["name"],
+                game["players"]["black"]["rating"],
+            )
+        except KeyError as e:
+            raise ParserError(e)
+
+        for i, move in enumerate(moves):
+            move.side = Color.WHITE if i % 2 == 0 else Color.BLACK
+
+        if white.username.lower() == username.lower():
+            side = Color.WHITE
+        elif black.username.lower() == username.lower():
+            side = Color.BLACK
+        else:
+            raise ParserError("Invalid username")
+
+        initial_fen = game.get("initialFen")
+        if initial_fen is None:
+            initial_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+
+        return Game(
+            game_id=game["id"],
+            platform=Platform.LICHESS,
+            url=f"https://lichess.org/{game["id"]}",
+            white=white,
+            black=black,
+            side=side,
+            initial_fen=initial_fen,
+            moves=moves,
+        )
+
+
 async def main():
     # testing
-    fetcher = ChessComFetcher()
-    games = await fetcher.fetch("hikaru", 1721088000)  # since July 16, 2024
+    # chesscom_fetcher = ChessComFetcher()
+    # chesscom_games = await chesscom_fetcher.fetch("hikaru", 1721088000)  # since July 16, 2024
 
-    parser = ChessComGameParser()
-    for game in games[:5]:
-        print(parser.parse(game, "hikaru"), end="\n\n")
+    # chesscom_parser = ChessComGameParser()
+    # for game in chesscom_games[:5]:
+    #     print(chesscom_parser.parse(game, "hikaru"), end="\n\n")
+
+    lichess_fetcher = LichessFetcher()
+    lichess_parser = LichessGameParser()
+    lichess_games = await lichess_fetcher.fetch("czarnov", 1723208004, 1723380804)
+    for game in lichess_games:
+        print(lichess_parser.parse(game, "czarnov"))
 
 
 if __name__ == "__main__":
     from asyncio import run
-    from ..fetcher import ChessComFetcher
+    from ..fetcher import ChessComFetcher, LichessFetcher
 
     run(main())
