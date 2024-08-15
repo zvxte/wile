181b94058abee1be6ad124d7dd9c0d9733eb8df0
zvxte <zvxte.codes@gmail.com>
2024-08-15 22:41:53 +0200

    create database interface

---
+++ src/database/database.py
@@ -0,0 +1,13 @@
+from typing import Protocol
+
+
+class Database(Protocol):
+    """
+    Database Interface
+    """
+
+    async def connect(self) -> None:
+        raise NotImplementedError
+
+    async def disconnect(self) -> None:
+        raise NotImplementedError
