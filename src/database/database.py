522f498353b452ea41c5a252b477bf1762461b8d
zvxte <zvxte.codes@gmail.com>
2024-08-23 22:36:20 +0200

    rename to DatabaseManager, add documentation

--- src/database/database.py
+++ src/database/database.py
@@ -1,16 +1,31 @@
-from typing import Protocol
+from typing import Protocol, Any
 
 
-class Database(Protocol):
+class DatabaseManager(Protocol):
     """
-    Database Interface
+    Database Manager Interface
+
+    Database manager implementations are responsible for managing
+    database connections (opening and closing connections).
+    Additionally, they handle the creation of tables, ensuring
+    that the database is properly set up.
     """
 
     async def connect(self) -> None:
+        """Connects to the database"""
         raise NotImplementedError
 
     async def disconnect(self) -> None:
+        """Disconnects from the database"""
+        raise NotImplementedError
+
+    def connection(self) -> Any:
+        """
+        Returns:
+            Any: Connection to the database. Type depends on a specific implementation.
+        """
         raise NotImplementedError
 
     async def create_tables(self) -> None:
+        """Creates tables"""
         raise NotImplementedError
