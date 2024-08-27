62e488909353ae05b2ca91a9cb20b729c9f52725
zvxte <zvxte.codes@gmail.com>
2024-08-27 21:40:30 +0200

    add unfinished chessboard_move_uci function

--- src/chess/chess_c/chessboard.c
+++ src/chess/chess_c/chessboard.c
@@ -3,8 +3,20 @@
 #include "status.h"
 
 Status_t chessboard_init_from_fen(char *fen, Chessboard_t *chessboard) {
+    if (!fen || !chessboard) {
+        return STATUS_ERROR;
+    }
+
     Position_t position;
     Status_t status = position_init_from_fen(fen, &position);
     chessboard->position = position;
     return status;
+};
+
+Status_t chessboard_move_uci(Chessboard_t *chessboard, char *uci_move) {
+    if (!chessboard || !uci_move) {
+        return STATUS_ERROR;
+    }
+    // todo
+    return STATUS_SUCCESS;
 };
