3dfe1e28ad6df6f8037ca467486048ef52b976ce
zvxte <zvxte.codes@gmail.com>
2024-09-14 21:39:51 +0200

    remove print statement

--- src/server/api_routes.py
+++ src/server/api_routes.py
@@ -4,7 +4,6 @@ from starlette.responses import JSONResponse
 
 
 async def health_check(request: Request):
-    print(request.query_params.getlist("platform"))
     return JSONResponse(
         {
             "status": 200,
